0303eeb33821a1cb096c60393aa27693a5a6530f
jamilngoding <batununggal143@gmail.com>
2019-01-24 21:31:22 +0700

    Vertex rocksssss!!!!

---
+++ app/src/test/java/com/vertextechnologies/magic/MainActivityTest.java
@@ -0,0 +1,11 @@
+package com.vertextechnologies.magic;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+
+}
